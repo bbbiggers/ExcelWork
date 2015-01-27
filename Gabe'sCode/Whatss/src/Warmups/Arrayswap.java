0089050d0066df5ebfd7eb5458bdca094095b388
Kinggabe <s-genberg@lwsd.org>
2015-01-27 08:34:03 -0800

    yup

---
+++ Gabe'sCode/Whatss/src/Warmups/Arrayswap.java
@@ -0,0 +1,25 @@
+package Warmups;
+
+public class Arrayswap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		String[] words = { "apple", "banana", "carrot" };
+		swap(words);
+		
+	}
+	public static void swap(String[]words){
+		int a = words.length;
+		
+		
+		
+	}
+		
+		
+		
+		
+	
+
+}
