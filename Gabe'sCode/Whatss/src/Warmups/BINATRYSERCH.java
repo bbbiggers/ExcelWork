f5ef343178ab244d537c0b72ec04943a8d8b2fe9
Kinggabe <s-genberg@lwsd.org>
2015-02-26 09:09:34 -0800

    helo

--- Gabe'sCode/Whatss/src/Warmups/BINATRYSERCH.java
+++ Gabe'sCode/Whatss/src/Warmups/BINATRYSERCH.java
@@ -1,21 +1,39 @@
 package Warmups;
 
+import java.util.ArrayList;
+
 public class BINATRYSERCH {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
-	public static int search (int [] array){
+	public static int search81 (ArrayList<Integer> newlist, int lo,int hi ){
 	
-	for
-
-	
-	
+	int mid = (lo+hi)/2;	
+		
+	if(lo>hi){
+		return -1;
+	}else if (newlist.get(mid)==81){
+		return mid;
+	}else if (newlist.get(mid)<81){
+		return search81(newlist,mid+1,hi);
+	}else{
+		return search81(newlist,lo,mid-1);
+	}
 	
 	
 	}
-	
+	/*
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 */
 	
 	
 	
