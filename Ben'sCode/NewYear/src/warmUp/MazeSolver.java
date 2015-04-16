79ff9da19663db702cbb6bd989555ea707d20c59
bbbiggers <bbbiggers@gmail.com>
2015-04-16 08:54:51 -0700

    tftg

---
+++ Ben'sCode/NewYear/src/warmUp/MazeSolver.java
@@ -0,0 +1,18 @@
+package warmUp;
+
+public class MazeSolver {
+
+	String[][] Ar;
+	int locR = 0;
+	int locC = 0;
+	public MazeSolver(String[][] Ar) {
+		Ar = this.Ar;
+	}
+	public int move() { //  0 = up   1 = down    2 = Right  3 = out!
+	int move = 0;
+	if(Ar.length == locR) {
+		
+	}
+		return move;
+	}
+}
