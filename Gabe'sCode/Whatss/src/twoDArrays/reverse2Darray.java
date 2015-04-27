a3c5cd2895df1f38337a00611986b3634fb61676
Kinggabe <s-genberg@lwsd.org>
2015-04-27 11:38:35 -0700

    k

---
+++ Gabe'sCode/Whatss/src/twoDArrays/reverse2Darray.java
@@ -0,0 +1,18 @@
+package twoDArrays;
+
+public class reverse2Darray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//int[][] myArray = new int[3][3];
+		
+		int[][] 
+				myArray = {	{0, 1, 2},
+							{3, 4, 5},
+							{6, 7, 8}	};
+					
+		
+	}
+
+}
