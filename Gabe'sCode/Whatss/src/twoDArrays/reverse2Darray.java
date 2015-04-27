0fd3b8c5330d6777a24d56e112e894c2f74103fd
Kinggabe <s-genberg@lwsd.org>
2015-04-27 12:29:17 -0700

    hiop

--- Gabe'sCode/Whatss/src/twoDArrays/reverse2Darray.java
+++ Gabe'sCode/Whatss/src/twoDArrays/reverse2Darray.java
@@ -7,12 +7,28 @@ public class reverse2Darray {
 
 		//int[][] myArray = new int[3][3];
 		
-		int[][] 
-				myArray = {	{0, 1, 2},
-							{3, 4, 5},
-							{6, 7, 8}	};
+		int[][] myArray = {	{1, 2, 3},
+							{4, 5, 6},
+							{7, 8, 9}	};
+		
+	}
+		public static int[][] diags(int[][] a){
+			
+			if(a.length!=a[0].length){
+				return null;
+			}
+			
+			
+			return a;
 					
+		}
+			
+			
+		
+	
+		
+		
 		
 	}
 
-}
+
